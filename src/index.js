6f8aefb48e01d24bc7971c9a3a3a1753b6892c64
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-08-08 20:45:10 +1000

    rendered existing toys to the dom and activatedadd toy functionality with POST method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +13,61 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //Fetch Request
+  function getAllToys(){
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toys => toys.forEach((toy) => renderOneToy(toy)))
+  }
+  getAllToys()
+
+ 
+
+
+  //Build Toy Card
+  function renderOneToy(toy){
+    let toyCard = document.createElement('div')
+    toyCard.className = 'card'
+    toyCard.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img class="toy-avatar" src= "${toy.image}" />
+    <p> ${toy.likes} Likes </p>
+    <button class="like-btn" id"${toy.id}> Like ❤️ </button>    
+    `
+    //Add animal card to DOM
+    document.querySelector('#toy-collection').appendChild(toyCard)
+  }
+
+  //add toy via form
+  //add event listener to form
+  document.querySelector('.add-toy-form').addEventListener('submit', handleSubmit)
+
+  //render new toy using form submission
+  function handleSubmit(e){
+    e.preventDefault()
+    let newToy = {
+      name:e.target.name.value,
+      image:e.target.image.value,
+      likes:0
+    };
+    console.log(newToy);
+    renderOneToy(newToy);
+    addNewToy(newToy);
+  } 
+
+  function addNewToy(newToy) {
+   fetch('http://localhost:3000/toys',{
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body:JSON.stringify(newToy)
+     })
+    .then(res => res.json())
+  }
+
+
+
 });
